95a740d1766187f63e1f45d30a695d9a6de253c8
Felix Cervin <fexellofficial@gmail.com>
2025-08-12 13:50:16 +0200

    docs: Added comments to User.route.js

--- routes/User/User.route.js
+++ routes/User/User.route.js
@@ -9,16 +9,25 @@ import AuthMiddleware from '../../middlewares/Auth.middleware.js'
  * @description User routes
  * @constant UserRouter
  * 
- * 
+ * @property {GET} / - Retrieves the user's information.
+ * @property {POST} / - The controller function responsible for creating a user, based on the user's input.
+ * @property {PUT} / - The controller function responsible for updating a user, based on the user's input.
+ * @property {GET} /all - Retrieves all users in the database.
+ * @property {GET} /username/:username - Retrieves a user by their username.
+ * @property {GET} /email/:email - Retrieves a user by their email.
+ * @property {GET} /:userId - Retrieves a user by their ID.
  */
 const UserRouter                            = Router()
 
+// /user routes first
 UserRouter.get('/', [
   AuthMiddleware.Authenticate,
   AuthMiddleware.RevokedRefreshToken,
   AuthMiddleware.EmailVerified,
   AuthMiddleware.AccountInactive,
 ], UserController.Get)
+
+UserRouter.post('/', UserController.Create)
 
 UserRouter.put('/', [
   AuthMiddleware.Authenticate,
@@ -27,6 +36,7 @@ UserRouter.put('/', [
   AuthMiddleware.AccountInactive,
 ], UserController.Update)
 
+// /user/all routes
 UserRouter.get('/all', [
   AuthMiddleware.Authenticate,
   AuthMiddleware.RevokedRefreshToken,
@@ -35,6 +45,7 @@ UserRouter.get('/all', [
   // AuthMiddleware.RoleChecker([ 'moderator', 'admin' ]),
 ], UserController.GetAll)
 
+// /user/username routes
 UserRouter.get('/username/:username', [
   AuthMiddleware.Authenticate,
   AuthMiddleware.RevokedRefreshToken,
@@ -42,6 +53,7 @@ UserRouter.get('/username/:username', [
   AuthMiddleware.AccountInactive,
 ], UserController.GetByUsername)
 
+// /user/email routes
 UserRouter.get('/email/:email', [
   AuthMiddleware.Authenticate,
   AuthMiddleware.RevokedRefreshToken,
@@ -50,6 +62,7 @@ UserRouter.get('/email/:email', [
   AuthMiddleware.RoleChecker([ 'moderator', 'admin' ]),
 ], UserController.GetByEmail)
 
+// /user/:userId routes
 UserRouter.get('/:userId', [
   AuthMiddleware.Authenticate,
   AuthMiddleware.RevokedRefreshToken,
@@ -57,8 +70,6 @@ UserRouter.get('/:userId', [
   AuthMiddleware.AccountInactive,
   // AuthMiddleware.RoleChecker([ 'moderator', 'admin' ]),
 ], UserController.GetByUserId)
-
-UserRouter.post('/', UserController.Create)
 
 export {
   UserRouter as default,
